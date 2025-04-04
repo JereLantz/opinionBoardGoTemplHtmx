c7cfc3e602694b6ef711a981e36169e2398c05e4
Jere Lantz <lantz.jere@gmail.com>
2025-04-04 19:31:06 +0300

    implemented hello world

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
-package main;
+package main
 
-func main(){
+import (
+	"log"
+	"net/http"
+)
+
+func handleRoot(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/"{
+		w.WriteHeader(404)
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Write([]byte("Hello world!"))
+}
+
+func main() {
+	handler := http.NewServeMux();
+	server := http.Server{
+		Addr: ":42069",
+		Handler: handler,
+	}
+
+	handler.HandleFunc("GET /", handleRoot)
+
+	log.Printf("http server started on port %s\n", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
