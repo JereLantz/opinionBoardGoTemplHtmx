eea4db2880aec0bef13a9dd8ccf1d54837a8e0f8
Jere Lantz <lantz.jere@gmail.com>
2025-04-10 22:15:26 +0300

    created the database connection and schema

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module opinionBoardGoTemplHtmx
 go 1.24.1
 
 require github.com/a-h/templ v0.3.857
+
+require github.com/mattn/go-sqlite3 v1.14.27 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"opinionBoardGoTemplHtmx/templates/home"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func handleRoot(w http.ResponseWriter, r *http.Request){
@@ -15,11 +18,50 @@ func handleRoot(w http.ResponseWriter, r *http.Request){
 	home.Index().Render(r.Context(), w)
 }
 
+func initializeDbScheme(db *sql.DB) error{
+	schemeInitQuery := `
+	CREATE TABLE IF NOT EXISTS opinions(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL,
+		opinion TEXT NOT NULL
+	);
+	`
+
+	_, err := db.Exec(schemeInitQuery)
+	if err != nil{
+		return err
+	}
+
+	return nil
+}
+func connectToDB() (*sql.DB, error){
+	db, err := sql.Open("sqlite3", "database.db")
+	if err != nil{
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	handler := http.NewServeMux();
 	server := http.Server{
 		Addr: ":42069",
 		Handler: handler,
+	}
+	db, err := connectToDB()
+	defer db.Close()
+	if err != nil {
+		log.Fatalf("error connecting to the database %s\n", err)
+	}
+
+	err = initializeDbScheme(db)
+	if err != nil {
+		log.Fatalf("could not initialize the database scheme %s\n",err)
 	}
 
 	handler.HandleFunc("GET /", handleRoot)
